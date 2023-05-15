a584bb485eae576278c08ef5fa8d5385c0a040aa
sSaNto <ssanto@localhost.localdomain>
2023-05-15 12:17:30 -0700

    cambiar por animacion pura a css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent {
+export class HomeComponent implements  OnInit, OnDestroy{
 
+  moveInterval: any;
+  windowWidth: number = 0;
 
+  left: number = 0;
+  top: number = 0;
+
+  leftStyle: string = "0px";
+  topStyle: string = "0px";
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.windowWidth = window.innerWidth;
+
+    this.moveInterval = setInterval(() => {
+
+      this.left += 100;
+      // this.top += 50;
+
+      console.log(this.windowWidth)
+      console.log(this.left)
+
+      if (this.windowWidth < this.left){
+        this.left = 0;
+      }
+
+      this.leftStyle = `${this.left}px`;
+      this.topStyle = `${this.top}px`;
+
+    }, 2000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.moveInterval);
+  }
 
 }
