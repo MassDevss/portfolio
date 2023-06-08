bb4ba3614d07f9a09ae349e42db1a4bfe5d8bd4c
ManuelsSaNt <95022226+ManuelsSaNt@users.noreply.github.com>
2023-06-08 00:18:31 -0700

    little of responsive for image and title and fix and add more text for text boxes

--- src/app/components/text-box/text-box.component.ts
+++ src/app/components/text-box/text-box.component.ts
@@ -1,23 +1,44 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-text-box',
   templateUrl: './text-box.component.html',
   styleUrls: ['./text-box.component.sass']
 })
-export class TextBoxComponent implements OnInit{
+export class TextBoxComponent implements OnInit, AfterViewInit{
 
   @Input()
   textTitle: string = "Default";
 
   @Input()
-  textContent: string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus at et ex officia quia? Amet deserunt eum illo labore nesciunt optio ratione, ut voluptatibus.";
+  textContent: string = "Default";
 
-  constructor() {
+  @ViewChild('textTitle')
+  titleElement: ElementRef<HTMLHeadElement> | undefined;
+
+  @ViewChild('textContent')
+  textElement: ElementRef<HTMLParagraphElement> | undefined;
+
+  constructor() { }
+
+  processText(toProcess: string): string {
+    let finalText = toProcess;
+    const regEx = /['\n']/g;
+    finalText = finalText.replace(regEx, '<br>');
+    return finalText;
   }
 
   ngOnInit(): void {
+    this.textTitle = this.processText(this.textTitle);
+    this.textContent = this.processText(this.textContent);
+  }
 
+  ngAfterViewInit(): void {
+    // @ts-ignore
+    this.titleElement.nativeElement.innerHTML = this.textTitle;
+
+    // @ts-ignore
+    this.textElement.nativeElement.innerHTML = this.textContent;
   }
 
 }
