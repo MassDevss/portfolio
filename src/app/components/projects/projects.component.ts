43ffd203289e2586922984b6343614e41715c035
Manuel Samaniego <samaniegoserranom@gmail.com>
2023-10-19 18:39:51 -0700

    add boostrap and quit resume from projects and add image of each tech used in the project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -12,30 +12,26 @@ export class ProjectsComponent {
     {
       url: "https://eltiburonburger.vercel.app/home",
       image: "tibuProject.webp",
-      name: "El Tiburón pagina web",
-      resume: "El Tiburón Burger es un restaurant de hamburguesas gurmet quien buscaba una pagina para mostrarse al mundo y dar un vistazo de los productos que crean",
-      technologies: ["Angular", "NodeJS", "MongoDB", "ExpressJS"]
+      name: "El Tiburón Burger web menu",
+      technologies: ["React", "MySQL", "NodeJS", "TypeScript", "Express"]
     },
     {
       url: "https://github.com/ManuelsSaNt/TiburonPVETs",
       image: "tiburon_sp.webp",
-      name: "El Tiburón punto de venta",
-      resume: "Con la necesidad de agilizar el proceso de tomar un pedido, procesarlo y entregarlo satisfactoriamente a los clientes se decidió crear este software, utilizado para cobrar y registrar los pedidos que tenga el negocio.",
-      technologies: ["Angular", "NodeJS", "MongoDB", "ExpressJS"]
+      name: "El Tiburón Burger Punto de venta",
+      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap", "Nodejs", "Electron", "MySQL"]
     },
     {
       url: "https://labstudiomedia.com",
       image: "labstudiomedia.webp",
-      name: "LabStudioMedia",
-      resume: "LabStudioMedia es un estudio de diseño gráfico, contenido audiovisual y desarrollo web que buscaba una pagina para mostrarse al mundo y dar un vistazo de los productos que crean.",
-      technologies: ["Angular", "NodeJS", "MongoDB", "ExpressJS"]
+      name: "LabStudioMedia.com",
+      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"]
     },
     {
       url: "https://www.cybercafe10m.com",
       image: "cybercafe10m.webp",
-      name: "CyberCafe10M",
-      resume: "",
-      technologies: ["Angular", "NodeJS", "MongoDB", "ExpressJS"]
+      name: "CyberCafe10M.com",
+      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"]
     }
   ];
 
