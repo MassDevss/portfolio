aba05343428a431dcfc13dcf8b2ac62d310f9581
ManuelSaNt <samaniegoserranom@gmail.com>
2023-11-09 17:30:41 -0700

    added links in projects, now the mail is with my domain

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -10,28 +10,32 @@ export class ProjectsComponent {
 
   projects: Project[] = [
     {
-      url: "https://eltiburonburger.vercel.app/home",
+      production: "https://eltiburonburger.vercel.app/home",
+      github: "",
       image: "tibuProject.webp",
       name: "El Tiburón Burger web menu",
       technologies: ["React", "MySQL", "NodeJS", "TypeScript", "Express"],
       description: 'Menu digital para restaurante, con sistema de administración de productos y sistema de login con usuarios.'
     },
     {
-      url: "https://github.com/ManuelsSaNt/TiburonPVETs",
+      production: "",
+      github: "https://github.com/ManuelsSaNt/TiburonPVETs",
       image: "tiburon_sp.webp",
       name: "El Tiburón Burger Punto de venta",
       technologies: ["HTML", "CSS", "JavaScript", "Bootstrap", "Nodejs", "Electron", "MySQL"],
       description: 'Punto de venta para restaurante, con sistema de administración de productos, registro de productos y clientes en el sistema.'
     },
     {
-      url: "https://labstudiomedia.com",
+      production: "https://labstudiomedia.com",
+      github: "",
       image: "labstudiomedia.webp",
       name: "LabStudioMedia.com",
       technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
       description: 'Sitio web para empresa de marketing digital, diseño y desarrollo web.'
     },
     {
-      url: "https://www.cybercafe10m.com",
+      production: "https://www.cybercafe10m.com",
+      github: "",
       image: "cybercafe10m.webp",
       name: "CyberCafe10M.com",
       technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
--- src/app/types/Project.type.ts
+++ src/app/types/Project.type.ts
@@ -1,11 +1,11 @@
 
 
 export type Project = {
-  url: string;
   image: string;
   name: string;
   description: string;
   technologies: string[];
   github?: string;
+  production?: string;
 }
 
